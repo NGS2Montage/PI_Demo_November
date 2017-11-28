5fe83cee7e88c9d4e6372199ca2a0da42222689f
Nathan Self <nwself@cs.vt.edu>
2017-11-27 19:29:41 -0500

    Logit

--- site/recsys/views.py
+++ site/recsys/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import json
+import logging
 
 from django.shortcuts import render
 from django.http import JsonResponse, Http404
@@ -9,6 +10,9 @@ from django.http import JsonResponse, Http404
 from keyword_test import compute_scores
 
 from .models import CitationObject
+
+
+logger = logging.getLogger('django.SIKE.recsys.views')
 
 
 def index(request):
@@ -38,8 +42,8 @@ def scores(request):
     cite_obj = CitationObject.objects.get_or_build(request.GET['doi'])
     cite_data = json.loads(cite_obj.cite_data)
     scores = compute_scores(cite_data, cite_obj.doi)
-    print("*******SCORES*******")
-    print(scores)
-    print("*******SCORES*******")
+    logger.debug("*******SCORES*******")
+    logger.debug(scores)
+    logger.debug("*******SCORES*******")
 
-    return JsonResponse(scores)
+    return JsonResponse({"scores": scores})
