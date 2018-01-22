07e7c91673d1a4c4537bcfcbaa794697169484ef
Nathan Self <nwself@cs.vt.edu>
2018-01-22 12:41:11 -0500

    Use env for database url so we can use postgres in production

--- site/montage/settings.py
+++ site/montage/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'montage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(default='sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
 }
 
 
