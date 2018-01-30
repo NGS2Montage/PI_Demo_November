5be837987378de5119a32ef804f8997b6913cae0
Nathan Self <nwself@cs.vt.edu>
2018-01-30 08:42:24 -0500

    Add level 2 fetcher

---
+++ site/recsys/scripts/fetch_level_2.py
@@ -0,0 +1,13 @@
+from recsys.models import Paper
+
+
+def run(doi='10.1.1.598.8940'):
+    p = Paper.object.get(doi=doi)
+
+    for c in p.citations.all():
+        for cc in c.citations.filter(fetched=False):
+            if not cc.fetched:
+                print("Might need {}".format(cc))
+                follow_citation(cc)
+            else:
+                print("Do not need {}".format(cc))
