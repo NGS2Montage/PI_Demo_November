77d0fd910501ccb942111898c7194c56f4bb5881
Nathan Self <nwself@cs.vt.edu>
2018-03-07 09:17:44 -0500

    Don't crash if a fetch co-cites call crashes

--- site/recsys/scripts/fetch_co_citations.py
+++ site/recsys/scripts/fetch_co_citations.py
@@ -4,4 +4,7 @@ from recsys.forms import get_co_citations
 
 def run(doi=None):
     for paper in Paper.objects.filter(fetched=True, fetched_co_citations=False):
-        get_co_citations(paper)
+        try:
+            get_co_citations(paper)
+        except Exception as e:
+            print("This didn't work for some reason, carrying on anyway")
